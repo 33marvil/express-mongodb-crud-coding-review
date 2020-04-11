9e10710a630b3114bac09a1d9e48797baed13375
33marvil <martinramirezcruz71@gmail.com>
2020-04-11 16:40:16 -0500

    Companies-POST

--- src/controllers/companies/companies.js
+++ src/controllers/companies/companies.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 
 
+
+
 const Company = require('../../models/Company');
 console.log(Company, 'out company');
 
 const controller = {
     index: (req, res) => {
-        console.log("inside index");
+        // console.log("inside index");
         Company
             .find()
             .exec()
@@ -22,6 +24,31 @@ const controller = {
                 console.log(`caugth error: ${err}`);
                 return res.status(500).json(err);
             });
+    },
+    create: (req, res) => {
+        console.log(Company, "inside create company");
+        const name = req.body.name;
+        console.log(name, 'req.body.name');
+
+        const newCompany = new Company({ // prototype
+            _id: mongoose.Types.ObjectId(),
+            name: name
+        });
+        console.log(newCompany, 'inside newCompany');
+        newCompany
+            .save()
+            .then(data => {
+                res
+                    .json({
+                        type: 'Company created',
+                        data: data
+                    })
+                    .status(200)
+            })
+            .catch(err => {
+                console.log(`caugth error: ${err}`);
+                return res.status(500).json(err);
+            })
     }
 
 }
--- src/routes/api.js
+++ src/routes/api.js
@@ -2,12 +2,18 @@ const { Router } = require("express");
 const app = Router();
 
 
+
+
 // testing model User, Appointments, Treatments
 const Companies = require('../controllers/companies/companies');
 console.log(Companies, "this companies");
 
 //companies GET
 app.get('/companies', Companies.index);
+
+
+// companies POST
+app.post('/companies', Companies.create);
 
 
 
